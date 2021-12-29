bc9f0ae467f49ea7e8ae12e345236298d8c56394
Pavan_PavanKumarA <pavan@leewayhertz.com>
2021-12-29 19:56:22 +0530

    created ui of successfully token created

---
+++ src/modules/createdToken/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import BaseComponent from "../baseComponent";
+import CreateToken from './createToken';
+
+
+class CreateTokenComponent extends BaseComponent {
+  render() {
+    return (
+      <>
+        <CreateToken state={this.state} />
+      </>
+    );
+  }
+}
+
+export default CreateTokenComponent;
