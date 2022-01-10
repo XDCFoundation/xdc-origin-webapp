6195ffbf72c96e23b32944349fa3cd0fb60f38a4
Abhey_AbheyGupta <abhey@leewayhertz.com>
2022-01-10 11:21:18 +0530

    updated changes

--- src/modules/createTokenXRC20/commonTab.js
+++ src/modules/createTokenXRC20/commonTab.js
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import BasicInfoPage from "./basicInformation";
 import TokenomicsPage from "./tokenomics";
-import AddFeaturesPage from './addFeature';
+import AddFeaturesPage from "./addFeature";
 import DeployContractPage from "./deployContract";
 
 const MainContainer = styled.div`
   display: flex;
   justify-content: center;
   background: #ecf0f7 0% 0% no-repeat padding-box;
-  height: 100%;
+  /* height: 1080px; */
 `;
 
 const Parent = styled.div`
@@ -54,13 +54,14 @@ const RowOne = styled.div`
 const Div = styled.div`
   display: flex;
   flex-direction: row;
+  cursor: pointer;
   width: 224.5px;
   border-bottom: 1px solid #f0f0f0;
   @media (min-width: 767px) and (max-width: 1024px) {
     width: 180.5px;
   }
   @media (min-width: 0px) and (max-width: 767px) {
-    width: 88.75px;
+    /* width: 88.75px; */
   }
 `;
 
@@ -101,6 +102,16 @@ const Description = styled.div`
 const TextOne = styled.div`
   font: normal normal normal 12px/15px Inter;
   letter-spacing: 0px;
+  color: #4b4b4b;
+  opacity: 1;
+  @media (min-width: 0px) and (max-width: 767px) {
+    display: none;
+  }
+`;
+
+const ActiveTextOne = styled.div`
+  font: normal normal normal 12px/15px Inter;
+  letter-spacing: 0px;
   color: #0089ff;
   opacity: 1;
   @media (min-width: 0px) and (max-width: 767px) {
@@ -111,7 +122,7 @@ const TextOne = styled.div`
 const TextTwo = styled.div`
   font: normal normal 600 17px/20px Inter;
   letter-spacing: 0px;
-  color: #0089ff;
+  color: #4b4b4b;
   opacity: 1;
   @media (min-width: 768px) and (max-width: 1024px) {
     white-space: nowrap;
@@ -123,30 +134,52 @@ const TextTwo = styled.div`
   }
 `;
 
+const ActiveTextTwo = styled.div`
+  font: normal normal 600 17px/20px Inter;
+  letter-spacing: 0px;
+  color: #0089ff;
+  opacity: 1;
+  @media (min-width: 768px) and (max-width: 1024px) {
+    white-space: nowrap;
+    font: normal normal 600 16px/20px Inter;
+    padding: 0px 10px 0px 0px;
+  }
+  @media (min-width: 0px) and (max-width: 767px) {
+    /* white-space: nowrap; */
+    font: normal normal 600 16px/20px Inter;
+    padding: 0px 10px 0px 0px;
+  }
+`;
+
 export default function CommonTab(props) {
+  const [opt, setOpt] = useState("1");
   const tab = [
     {
       id: 1,
       step: "Step 1",
       image: "/images/ContractDetails.svg",
+      image2: "/images/Selected-Circle.svg",
       name: "Basic Information",
     },
     {
       id: 2,
       step: "Step 2",
       image: "/images/Tokenomics.svg",
+      image2: "/images/Selected-Circle.svg",
       name: "Tokenomics",
     },
     {
       id: 3,
       step: "Step 3",
       image: "/images/Features.svg",
+      image2: "/images/Selected-Circle.svg",
       name: "Add Features",
     },
     {
       id: 4,
       step: "Step 4",
       image: "/images/Deploy.svg",
+      image2: "/images/Selected-Circle.svg",
       name: "Deploy Contract",
     },
   ];
@@ -159,26 +192,54 @@ export default function CommonTab(props) {
           <Column>
             <RowOne>
               {tab.map((item) => {
+                const TextOneActive = opt == item.id ? ActiveTextOne : TextOne;
+                const TextTwoActive = opt == item.id ? ActiveTextTwo : TextTwo;
                 return (
                   <>
                     <Div key={item.id}>
-                      <ImageDiv>
-                        <Img alt="" src={item.image} />
+                      <ImageDiv
+                        id={item.id}
+                        onClick={(e) => setOpt(e.currentTarget.id)}
+                      >
+                        <Img
+                          alt=""
+                          src={item.image}
+                        />
                       </ImageDiv>
 
                       <Description>
-                        <TextOne>{item.step}</TextOne>
-                        <TextTwo>{item.name}</TextTwo>
+                        <TextOneActive
+                          id={item.id}
+                          onClick={(e) => setOpt(e.currentTarget.id)}
+                        >
+                          {item.step}
+                        </TextOneActive>
+                        <TextTwoActive
+                          id={item.id}
+                          onClick={(e) => setOpt(e.currentTarget.id)}
+                        >
+                          {item.name}
+                        </TextTwoActive>
                       </Description>
                     </Div>
                   </>
                 );
               })}
             </RowOne>
-            {/* <BasicInfoPage />
-            <TokenomicsPage />
-            <AddFeaturesPage/> */}
-            <DeployContractPage/>
+            {(() => {
+              switch (opt) {
+                case "1":
+                  return <BasicInfoPage />;
+                case "2":
+                  return <TokenomicsPage />;
+                case "3":
+                  return <AddFeaturesPage />;
+                case "4":
+                  return <DeployContractPage />;
+                default:
+                  return ;
+              }
+            })()}
           </Column>
         </Parent>
       </MainContainer>
