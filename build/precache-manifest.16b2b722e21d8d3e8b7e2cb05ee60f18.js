60048521e66057f685a9ace0d5220d097728872f
Abhey_AbheyGupta <abhey@leewayhertz.com>
2022-01-24 16:12:43 +0530

    created build

---
+++ build/precache-manifest.16b2b722e21d8d3e8b7e2cb05ee60f18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "093c12513cc24329210361e08e55b24d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d81937b769c2f7b695f",
+    "url": "/static/css/2.abb14d58.chunk.css"
+  },
+  {
+    "revision": "09731075bd19e238e50a",
+    "url": "/static/css/main.05af64af.chunk.css"
+  },
+  {
+    "revision": "5d81937b769c2f7b695f",
+    "url": "/static/js/2.1af7bb86.chunk.js"
+  },
+  {
+    "revision": "09731075bd19e238e50a",
+    "url": "/static/js/main.c91f2e84.chunk.js"
+  },
+  {
+    "revision": "5478bda222d0aadc702f",
+    "url": "/static/js/runtime~main.d9759dd6.js"
+  }
+]);
