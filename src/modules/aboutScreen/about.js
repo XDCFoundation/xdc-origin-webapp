aec91f982fecd39adb939f9744f9d13230278029
Prathamesh_7 <prathamesh@leewayhertz.com>
2022-01-25 16:58:56 +0530

    Fixed create xrc20 button bug while user is not logged in

--- src/modules/aboutScreen/about.js
+++ src/modules/aboutScreen/about.js
@@ -78,11 +78,11 @@ function About(props) {
 
   const handleXDCPayWallet = async () => {
     if (
-      !props?.currentUser?.isLoggedIn &&
+      !props?.currentUser?.isLoggedIn ||
       props?.currentUser?.accountDetails === null
     ) {
       props.user(connectWallet);
-    } else {
+    } else if(props?.currentUser?.isLoggedIn){
       history.push("/token-XRC20");
       props.setActiveNavItem("create");
       props.setSubNavItem(true);
