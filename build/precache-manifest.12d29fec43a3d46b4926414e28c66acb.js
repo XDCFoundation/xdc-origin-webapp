06314dc89c4c4676d4fc92c2a03e8a659c035036
shivanib <shivanib@leewayhertz.com>
2022-03-14 19:26:22 +0530

    production build

--- build/precache-manifest.12d29fec43a3d46b4926414e28c66acb.js
+++ build/precache-manifest.12d29fec43a3d46b4926414e28c66acb.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "232196de952ca222f866917b8209284f",
+    "revision": "bd657f633530bc6bba422df358a1aa02",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.abb14d58.chunk.css"
   },
   {
-    "revision": "81f0cda789d3b5a047d3",
+    "revision": "cddbfebf2a50f156ce33",
     "url": "/static/css/main.df8449c8.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.3713cde6.chunk.js"
   },
   {
-    "revision": "81f0cda789d3b5a047d3",
-    "url": "/static/js/main.ee29fd97.chunk.js"
+    "revision": "cddbfebf2a50f156ce33",
+    "url": "/static/js/main.9aeed034.chunk.js"
   },
   {
     "revision": "5478bda222d0aadc702f",
