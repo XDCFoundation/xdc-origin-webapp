1c91453cbe5446dd7413b08e17c8a711e7028cf1
Prathamesh Gaikwad <prathamesh@leewayhertz.com>
2022-04-13 17:18:39 +0530

    QA Testing Build v1.0

---
+++ build/precache-manifest.3e919cb356dcbafbb7c652e2b165f749.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4473cb294c4261786ab1aa373676537d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73f7448b322aedf8f8ae",
+    "url": "/static/css/2.abb14d58.chunk.css"
+  },
+  {
+    "revision": "2218714296f826306c77",
+    "url": "/static/css/main.b616de0b.chunk.css"
+  },
+  {
+    "revision": "73f7448b322aedf8f8ae",
+    "url": "/static/js/2.8a1dff9f.chunk.js"
+  },
+  {
+    "revision": "2218714296f826306c77",
+    "url": "/static/js/main.c4e34d25.chunk.js"
+  },
+  {
+    "revision": "5478bda222d0aadc702f",
+    "url": "/static/js/runtime~main.d9759dd6.js"
+  }
+]);
