78af4c1c602b5e7ec0cf9ac2b0ad3f5b0550cb1f
shivanib <shivanib@leewayhertz.com>
2022-04-22 15:51:23 +0530

    Testing Build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c1dcd23a2fc12cfba41ac9a157bce3d.js"
+  "/precache-manifest.9a1252b8f297f60e8a2e497717cdb0e6.js"
 );
 
 self.addEventListener('message', (event) => {
